e3ba660dfd74d003d60cd05ee8b673a02135155d
Tianhe <federalkpl@gmail.com>
2017-06-11 15:18:26 -0400

    commented unused code in lab2 .

--- kern/init.c
+++ kern/init.c
@@ -29,17 +29,17 @@ i386_init(void)
 	// Lab 2 memory management initialization functions
 	mem_init();
 
-	// Some interesting outputs from lab1
-	int x = 1, y = 3, z = 4;
-	cprintf("x %d, y %x, z %d\n", x, y, z);
+	// // Some interesting outputs from lab1
+	// int x = 1, y = 3, z = 4;
+	// cprintf("x %d, y %x, z %d\n", x, y, z);
 
-	unsigned int i = 0x00646c72;
-    cprintf("H%x Wo%s\n", 57616, &i);
+	// unsigned int i = 0x00646c72;
+    // cprintf("H%x Wo%s\n", 57616, &i);
 
-	cprintf("x=%d y=%d\n", 3);
+	// cprintf("x=%d y=%d\n", 3);
 
-	// Test the stack backtrace function (lab 1 only)
-	test_backtrace(5);
+	// // Test the stack backtrace function (lab 1 only)
+	// test_backtrace(5);
 
 	// Drop into the kernel monitor.
 	while (1)
